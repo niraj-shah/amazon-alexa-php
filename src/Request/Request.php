c4d7389049a9d4668a7f27f7a1d835634cd47cd4
Niraj Shah <niraj3k@gmail.com>
2018-02-03 15:00:56 +0000

    Added Signature validation

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -12,11 +12,15 @@ abstract class Request {
 	public $requestId;
 	public $timestamp;
 	public $user;
+	public $cert;
+	public $signature;
 
 	public function __construct($data) {
 		$this->requestId = $data['request']['requestId'];
 		$this->timestamp = new DateTime($data['request']['timestamp']);
 		$this->user = new User($data['session']['user']);
+		$this->cert = $_SERVER['HTTP_SIGNATURECERTCHAINURL'] ?: null;
+		$this->signature = $_SERVER['HTTP_SIGNATURE'] ?: null;
 	}
 
 	public static function fromData($data) {
@@ -42,6 +46,20 @@ abstract class Request {
 
 		if ($differenceInSeconds > self::TIMESTAMP_VALID_TOLERANCE_SECONDS) {
 			throw new InvalidArgumentException('Request timestamp was too old. Possible replay attack.');
+		}
+	}
+
+	public function validateSignature($data = null) {
+		if (!$this->cert || !$this->signature) {
+			throw new RuntimeException('Request signature was not provided.');
+		}
+
+		$pem = file_get_contents($this->cert);
+		$pubKey = openssl_pkey_get_public($pem);
+		$verify = openssl_verify($data, base64_decode($this->signature), $pubKey, 'sha1');
+
+		if ($verify !== 1) {
+			throw new RuntimeException('Request signature was not valid.');
 		}
 	}
 }
